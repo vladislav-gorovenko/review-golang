c96cc32216fb02b299329843b4fdd25f83d96d4f
vladislav-gorovenko <vladislav.gorovenko.webdev@gmail.com>
2025-07-27 02:02:12 +0400

    2. currency converter

--- converter/converter.go
+++ converter/converter.go
@@ -1,8 +1,46 @@
 package converter
 
-func USDToRUB(usdInRub *float64, usd float64) {
-	USDtoEUR := 0.99
-	EURtoRUB := 100.0
-	USDtoRUB := USDtoEUR * EURtoRUB
-	*usdInRub = usd * USDtoRUB
+import "fmt"
+
+type Currency string
+
+const USDtoRUB float64 = 80
+
+const (
+	USD Currency = "USD"
+	RUB Currency = "RUB"
+)
+
+func GetCurrencyInput() (Currency, Currency) {
+	var fromCurrency Currency
+	var toCurrency Currency
+
+	fmt.Print("Type both from and to currency: ")
+	n, errCurrency := fmt.Scan(&fromCurrency, &toCurrency)
+	if n != 2 || errCurrency != nil {
+		return "USD", "RUB"
+	}
+
+	return fromCurrency, toCurrency
+}
+
+func GetAmountInput() float64 {
+	var amount float64
+
+	fmt.Print("Type amount: ")
+	_, errAmount := fmt.Scan(&amount)
+	if errAmount != nil {
+		return 0
+	}
+
+	return amount
+}
+
+func ConvertCurrency(from Currency, to Currency, amount *float64) {
+	if from == USD && to == RUB {
+		*amount = *amount * USDtoRUB
+	}
+	if from == RUB && to == USD {
+		*amount = *amount / USDtoRUB
+	}
 }
--- main.go
+++ main.go
@@ -3,48 +3,11 @@ package main
 import (
 	"fmt"
 	"github.com/vladislav-gorovenko/review-golang/converter"
-	"math"
 )
 
-func imt() {
-	var imt float64
-	fmt.Println("Calculator of IMT")
-	userWeight, userHeight := getWeightAndHeight()
-	calculateIMT(&imt, userWeight, userHeight)
-	printFinalResult(imt)
-}
-
-func calculateIMT(imt *float64, userWeight float64, userHeight float64) {
-	const IMTPower float64 = 2
-	*imt = userWeight / math.Pow(userHeight, IMTPower)
-}
-
-func printFinalResult(imt float64) {
-	result := fmt.Sprintf("Ваш индекс массы тела %.2f\n", imt)
-	fmt.Print(result)
-}
-
-func getWeightAndHeight() (weight float64, height float64) {
-	var userWeight float64
-	var userHeight float64
-
-	fmt.Print("Type your weight: ")
-	_, errWeight := fmt.Scan(&userWeight)
-	if errWeight != nil {
-		return
-	}
-	fmt.Print("Type your height: ")
-	_, errHeight := fmt.Scan(&userHeight)
-	if errHeight != nil {
-		return
-	}
-
-	return userWeight, userHeight
-}
-
 func main() {
-	var usdInRub float64
-	imt()
-	converter.USDToRUB(&usdInRub, 100)
-	fmt.Println(usdInRub)
+	var convertAmount = converter.GetAmountInput()
+	fromCurrency, toCurrency := converter.GetCurrencyInput()
+	converter.ConvertCurrency(fromCurrency, toCurrency, &convertAmount)
+	fmt.Printf("Converted amount: %.2f\n", convertAmount)
 }
